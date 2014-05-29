edde2104f96750e397f97431531b7d678e4a58e1
Ville Saarinen <villes@paju.oulu.fi>
2014-05-29 21:29:17 +0300

    Added class for brick handling

---
+++ BrickGenerator.java
@@ -0,0 +1,20 @@
+
+public class BrickGenerator 
+{
+
+	private int width;
+	private int height;
+	private int rows;
+	private int columns;
+	
+	public BrickGenerator(int width, int height, int rows, int columns)
+	{
+		this.width = width;
+		this.height = height;
+		this.rows = rows;
+		this.columns = columns;
+		
+	}
+	
+	
+}
