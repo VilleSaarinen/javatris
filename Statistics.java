e1e95db69a0d22483793130d7834c7509567028e
Ville Saarinen <villes@paju.oulu.fi>
2014-05-29 22:08:41 +0300

    Added class for game statistics.

---
+++ Statistics.java
@@ -0,0 +1,28 @@
+
+public class Statistics 
+{
+
+	private int level;
+	private int points;
+	
+	public Statistics()
+	{
+		level = 1;
+		points = 0;
+	}
+	
+	public int getLevel()
+	{
+		return level;
+	}
+
+	public int getPoints()
+	{
+		return points;
+	}
+	
+	
+	
+	
+	
+}
